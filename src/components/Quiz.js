0fff0b40d69bb992f54fc6f061a3ae8c59b2cbac
Jryke <rykerson@gmail.com>
2020-01-23 18:05:15 -0500

    redux added to fetch questions and manage state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import QuestionCard from './QuestionCard'
+import { connect } from 'react-redux'
+import { fetchQuestions } from '../actions'
 
 class Quiz extends React.Component {
+  // fetch questions at initial page load
+  componentDidMount() {
+    this.props.fetchQuestions()
+  }
   render() {
+    // check that questions are being fetched
+    console.log(this.props.questions)
     return(
       <div className='ui center aligned container'>
         <h1>Title</h1>
@@ -13,4 +21,9 @@ class Quiz extends React.Component {
   }
 }
 
-export default Quiz
+// mapStateToProps to update props when redux state changes
+const mapStateToProps = (state) => {
+  return { questions: state.questions }
+}
+
+export default connect(mapStateToProps, { fetchQuestions })(Quiz)
