7538c81c44b0a21c0a2bd463081b9ef4dc25e53a
Jryke <rykerson@gmail.com>
2020-01-23 18:03:50 -0500

    axios api request created and used for action creator

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import questionJSON from '../apis/questionsJSON'
+
+export const fetchQuestions = () => {
+  // redux-thunk used to handle async/await with redux
+  return async (dispatch) => {
+    const questionsObj = await questionJSON.get('/api.php?amount=10&difficulty=hard&type=boolean')
+    // dispatch manually once object is fetched
+    dispatch({
+      type: 'FETCH_QUESTIONS',
+      payload: questionsObj.data.results
+    })      
+
+  }
+  
+}
