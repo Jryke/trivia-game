4fd7e5424e0f06dd16f31e865c61c796e239dbb6
Jryke <rykerson@gmail.com>
2020-01-23 11:30:44 -0500

    Intro page created with basic styling

---
+++ src/components/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div className='ui center aligned container'>
+      <h1>Welcome to the Trivia Challenge!</h1>
+      <p>You will be presented with 10 True or False questions.</p>
+      <p>Can you score 100%?</p>
+      <button className='ui button'>BEGIN</button>
+    </div>
+  )
+}
+
+export default Intro
