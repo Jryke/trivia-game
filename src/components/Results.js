ac303e8c85494df1c34489e70a0046f18bb282f2
Jryke <rykerson@gmail.com>
2020-01-25 16:17:17 -0500

    number correct and total questions rendering dynamically

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux'
 
 
 class Results extends React.Component {
+  numberCorrect = () => {
+    return this.props.answers.filter(answer => answer.isCorrect).length
+  }
   render() {
     console.log(this.props)
     return(
       <div className='ui center aligned container'>
         <h1>You Scored</h1>
-        <h2>3 / 10</h2>
+        <h2>{this.numberCorrect()} / {this.props.answers.length}</h2>
         <p><i className="green check icon"></i>Correct answer</p>
         <p><i className="red times icon"></i>wrong answer</p>
         <button className='ui button'>Play Again</button>
