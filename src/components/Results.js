cc34edb5c2ed81b8b717d8bbaff76acab4d62c95
Jryke <rykerson@gmail.com>
2020-01-25 16:06:38 -0500

    Results changed to class component, receiving questions and answers objects from Redux

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Results = () => {
-  return(
-    <div className='ui center aligned container'>
-      <h1>You Scored</h1>
-      <h2>3 / 10</h2>
-      <p><i className="green check icon"></i>Correct answer</p>
-      <p><i className="red times icon"></i>wrong answer</p>
-      <button className='ui button'>Play Again</button>
-    </div>
-  )
+
+class Results extends React.Component {
+  render() {
+    console.log(this.props)
+    return(
+      <div className='ui center aligned container'>
+        <h1>You Scored</h1>
+        <h2>3 / 10</h2>
+        <p><i className="green check icon"></i>Correct answer</p>
+        <p><i className="red times icon"></i>wrong answer</p>
+        <button className='ui button'>Play Again</button>
+      </div>
+    )
+  }
 }
 
-export default Results
+const mapStateToProps = (state) => {
+  return {
+    questions: state.questions,
+    answers: state.answers
+  }
+}
+
+export default connect(mapStateToProps)(Results)
