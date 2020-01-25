795003dbf6d9c59386d56172b3a3aa1d5088d92a
Jryke <rykerson@gmail.com>
2020-01-25 16:28:32 -0500

    map of answers working, rendering correct or incorrect icon

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,12 +6,22 @@ class Results extends React.Component {
   numberCorrect = () => {
     return this.props.answers.filter(answer => answer.isCorrect).length
   }
+  renderIcon = (answer) => {
+    return answer.isCorrect ? <p><i className="green check icon"></i>Correct answer</p> : <p><i className="red times icon"></i>Incorrect</p>
+  }
   render() {
     console.log(this.props)
     return(
       <div className='ui center aligned container'>
         <h1>You Scored</h1>
         <h2>{this.numberCorrect()} / {this.props.answers.length}</h2>
+        {
+          this.props.answers.map((answer, i) => {
+            return(
+              this.renderIcon(answer)
+            )
+          })
+        }
         <p><i className="green check icon"></i>Correct answer</p>
         <p><i className="red times icon"></i>wrong answer</p>
         <button className='ui button'>Play Again</button>
