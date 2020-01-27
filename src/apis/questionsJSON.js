062a19953261f83656195bbe9953a3d3074a54b9
Jryke <rykerson@gmail.com>
2020-01-26 21:33:08 -0500

    Updates

--- src/apis/questionsJSON.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-// use axios and set base URL for fetching data in action creators
-export default axios.create({
-  baseURL: 'https://opentdb.com'
-})
--- src/reducers/questionsReducer.js
+++
@@ -1,9 +0,0 @@
-// redux reducer
-export default (state = [], action) => {
-  switch (action.type) {
-    case 'FETCH_QUESTIONS':
-      return action.payload
-    default:
-      return state
-  }
-}
