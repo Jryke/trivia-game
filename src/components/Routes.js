f860e445b8726de04a20f4e2ddee0ba4327f3011
Jryke <rykerson@gmail.com>
2020-01-26 22:03:48 -0500

    redirect removed from react-router-dom imports

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Intro from './Intro'
 import Quiz from './Quiz'
 import Results from './Results'
