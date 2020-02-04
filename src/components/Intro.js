8c0bede5c95ef258f3fbbe4d9ed7914c3d3f07c9
Jryke <rykerson@gmail.com>
2020-02-03 19:54:57 -0500

    component converted to class component to use lifecycle methods

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Intro = () => {
-  return (
-    <div className='ui center aligned container'>
-      <h1>Welcome to the Trivia Challenge!</h1>
-      <p>You will be presented with 10 True or False questions.</p>
-      <p>Can you score 100%?</p>
-      <Link to={'/quiz'}><button className='ui button'>BEGIN</button></Link>
-    </div>
-  )
+class Intro extends React.Component {
+  render() {
+    return (
+      <div className='ui center aligned container'>
+        <h1>Welcome to the Trivia Challenge!</h1>
+        <p>You will be presented with 10 True or False questions.</p>
+        <p>Can you score 100%?</p>
+        <Link to={'/quiz'}><button className='ui button'>BEGIN</button></Link>
+      </div>
+    )
+  }
 }
 
 export default Intro
