6ec733f1a7898b8228a61a5137a3a2ddbb59bd0c
Jryke <rykerson@gmail.com>
2020-03-07 13:27:49 -0500

    fetchQuestions action updated to customize api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,24 @@ import questionJSON from '../apis/questionsJSON'
 
 export const fetchQuestions = () => {
   // redux-thunk used to handle async/await with redux
-  return async (dispatch) => {
-    const questionsObj = await questionJSON.get('/api.php?amount=10&difficulty=hard&type=boolean')
+  return async (dispatch, getState) => {
+    // console.log('fetchQuestions dispatch', dispatch)
+    const options = getState().options
+    console.log('fetchQuestions info', options)
+    let apiLink = `/api.php?amount=${options.number}`
+    if (options.category) {
+      apiLink = apiLink.concat('&category=', options.category)
+    }
+    if (options.difficulty) {
+      apiLink = apiLink.concat('&difficulty=', options.difficulty)
+    }
+    if (options.type) {
+      apiLink = apiLink.concat('&type=', options.type)
+    }
+    
+
+    console.log(apiLink)
+    const questionsObj = await questionJSON.get(apiLink)
     // dispatch manually once object is fetched
     dispatch({
       type: 'FETCH_QUESTIONS',
