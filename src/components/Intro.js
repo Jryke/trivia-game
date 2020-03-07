e46c7bd4b31166abb0f56c6c29d04df106028e46
Jryke <rykerson@gmail.com>
2020-03-07 14:07:12 -0500

    headers added to dropdowns, inline padding added below dropdowns

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -21,11 +21,12 @@ class Intro extends React.Component {
         <h1>Welcome to the Trivia Challenge!</h1>
         <p>You will be presented with 10 True or False questions.</p>
         <p>Select your game parameters:</p>
-        <div className='ui stackable grid'>
+        <div className='ui relaxed stackable grid' style={{paddingBottom: '1rem'}}>
           <div className='four column row'>
             <div className='four wide column'>
+              <h4>Category:</h4>
               <select className='ui dropdown' style={{width: '12rem'}} onChange={e => this.sendToRedux(e, 'category')}>
-                <option value=''>Category</option>
+                <option value=''>Any Category</option>
                 {this.props.categories.map(category => {
                   return(
                     <option value={category.id} key={category.id}>{category.name}</option>
@@ -34,21 +35,24 @@ class Intro extends React.Component {
               </select>
             </div>
             <div className='four wide column'>
+              <h4>Difficulty:</h4>
               <select className='ui dropdown' style={{width: '12rem'}} onChange={e => this.sendToRedux(e, 'difficulty')}>
-                <option value=''>Difficulty</option>
+                <option value=''>Any Difficulty</option>
                 <option value='easy'>Easy</option>
                 <option value='medium'>Medium</option>
                 <option value='hard'>Hard</option>
               </select>
             </div>
             <div className='four wide column'>
+              <h4>Question Type:</h4>
               <select className='ui dropdown' style={{width: '12rem'}} onChange={e => this.sendToRedux(e, 'type')}>
-                <option value=''>Type</option>
+                <option value=''>Any Type</option>
                 <option value='multiple'>Multiple Choice</option>
                 <option value='boolean'>True / False</option>
               </select>
             </div>
             <div className='four wide column'>
+              <h4>Number of Questions:</h4>
               <div className="ui input" style={{width: '12rem'}}>
                 <input type="number" placeholder="# of questions" value={this.props.options.number} min='1' max='50' onChange={e => this.sendToRedux(e, 'number')} />
               </div>
